d795c70e9e593b197ec9d74e6026166ce928b444
ethan <ethan1.jiang@gmail.com>
2015-08-02 11:49:46 +0800

    add db migration : init and remove old db

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 # manage.py
 
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 
-from app import app
+from app import app, db
+
+migrate = Migrate(app,db)
 
 manager = Manager(app)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def hello():
